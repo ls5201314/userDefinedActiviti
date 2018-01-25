37a6dc7c2385e9ee003e9944f9540644ffcd0b26
ls5201314 <626292171@qq.com>
2018-01-26 00:06:44 +0800

    Initial commit

---
+++ doc/database/formtest.sql
@@ -0,0 +1,26 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost
+Source Server Version : 50635
+Source Host           : localhost:3306
+Source Database       : testtu
+
+Target Server Type    : MYSQL
+Target Server Version : 50635
+File Encoding         : 65001
+
+Date: 2018-01-25 23:35:15
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for formtest
+-- ----------------------------
+DROP TABLE IF EXISTS `formtest`;
+CREATE TABLE `formtest` (
+  `formId` int(11) DEFAULT NULL,
+  `formType` varchar(50) DEFAULT NULL,
+  `form` blob
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
